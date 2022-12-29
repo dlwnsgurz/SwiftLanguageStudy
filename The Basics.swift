7cc985f7c78cce8c96cbbb361de88bb715507e07
LEE <lee@LEEui-MacBookPro.local>
2022-12-30 00:16:01 +0900

    The Basics

--- The Basics.swift
+++ The Basics.swift
@@ -156,3 +156,154 @@ let pi = Double(three) + pointOneFourOneFiveNine
 
 /// 부동 소수점을 정수로 변환하는 경우에는 무조건 버림 연산을 수행한다.
 let piInteger = Int(pi) // piInteger equals 3
+
+/*------------------------------------------*/
+
+/// - Type Alias
+
+/// 타입에 새로운 별칭을 부여할 수 있다.
+/// typealias 키워드를 통해 타입의 별칭을 지정할 수 있다.
+
+typealias AudioSample = UInt16
+var maxAmplitudeFound = AudioSample.min
+
+/*------------------------------------------*/
+
+/// - Booleans
+
+/// 변수나 상수를 선언할 때 스위프트의 타입 추론 기능으로 인해 타입 지정을 할 필요 없다.
+/*------------------------------------------*/
+
+/// - Tuples
+///
+/// 튜플은 주로 함수의 리턴값으로 여러 개의 값을 리턴할 때 유용하게 사용할 수 있다.
+///
+/// 튜플에서 개별의 값을 분해할 때 크게 3가지 방법이있다.
+let http404Error = (404, "Not Found")
+
+// 첫번째 방법, 언더케이스(_)로 불필요한 값은 분해하지 않을 수 있다.
+let (errorCode, descrption) = http404Error
+let (errorCodeRetrived, _) = http404Error
+
+// 두번째 방법, 인덱스를 통한 접근 방법
+let errorNumber = http404Error.0
+let errorMessage = http404Error.1
+
+// 세번째 방법, 선언시에 이름을 지어줄 수 있다.
+let http200Error = (statusCode: 200, description: "OK")
+
+let code = http200Error.statusCode
+let message = http200Error.description
+
+/*------------------------------------------*/
+
+/// - Optional
+///
+/// 스위프트에서 옵셔널은 모든 타입에 적용이 가능하다.
+/// 옵셔널은 다음 2가지를 의미한다.
+/// 첫번쨰, 값을 가지고 있다
+/// 두번째, 값을 가지고 있지 않다.
+
+/// convertNumber의 타입은 Int가 아닌 Int?로 추론된다.
+/// 문자열 "123" 은 123 리터럴로 변환이 가능하지만 "hello" 같은 경우에는
+/// 정수로 바꿀 수 없다. 따라서 nil이 들어갈 수 있도록
+/// 옵셔널로 타입 추론이 된다.
+let possibleNumber = "123"
+let convertedNumber = Int(possibleNumber)
+
+/// 옵셔널이 아닌 타입에는 nil이 들어갈 수 없다.
+/// 만약 초기값 없이 옵셔널 타입으로만 선언한다면 자동으로 nil이 저장된다.
+/// - Note: nil은 값이 아니라 값이 없는 상태를 의미한다.
+var serverResponseCode: Int? = 404
+serverResponseCode = nil
+
+/// nil이 저장된다.
+let serverAnswer: String?
+
+/// 옵셔널 타입에서 값을 추출하는 3가지 방법
+/// 첫번째는 if조건문과 강제 언래핑을 이용하는 방법으로
+/// 만약, nil이 들어있는 경우에 강제 언래핑을 한다면 런타임에러가 발생한다.
+if convertedNumber != nil{
+    print("convertNumber contains some integer")
+}
+
+if convertedNumber != nil{
+    print("convertedNumber is \(convertedNumber!)")
+}
+
+/// 두번째는 옵셔널 바인딩으로, if let 구문을 이용해 시행한다.
+/// 옵셔널 바인딩은 블록 내에서만 값의 변경이 가능하므로 guard구문과 차이가 있다.
+
+if let actualNumber = Int(possibleNumber){
+    print("possibleNumber is \(actualNumber)")
+}
+
+let myNumber = Int(possibleNumber)
+if let myNumber = myNumber{
+    print("my number is \(myNumber)")
+}
+/// 다음과 같이 줄일 수 있다.
+if let myNumber{
+    print("my number is \(myNumber)")
+}
+
+/// 옵셔널 바인딩은 조건문과 함께 사용할 수 있으며,
+/// 콤마(,)를 통해 구분한다.
+/// 또한 nil이나 조건이 false인 경우 탈출한다.
+if let firstNumber = Int("4"), let secondNumber = Int("123"), firstNumber < secondNumber{
+    print("\(firstNumber) < \(secondNumber)")
+}
+
+/// 상수뿐만 아니라 변수로도 옵셔널 바인딩이 가능하지만,
+/// 이 경우에는 원본값의 변경이 일어나지 않는다.
+if var mySecondNumber = myNumber{
+    print("available")
+}
+/// 세 번째는 암시적 추출 옵셔널이다.
+/// 암시적 추출 옵셔널은 타입명뒤에 ?가 아닌 !가 붙는다.
+/// 암시적 추출 옵셔널은 nil이 될 수 있는 타입이지만, nil이 되지 않을 확실한 상황인 경우에 사용한다.
+
+let possibleString: String? = "An Onptional String"
+let forcedString: String = possibleString!
+
+let assumedString: String! = "An implicitly unwrapped optional String"
+let implicitString: String = assumedString
+
+/// 스위프트는 암시적 추출 옵셔널을 처리하는 순서가 있다.
+/// 처음에는 반드시 암시적 추출 옵셔널도 일반 옵셔널으로 처리한다.
+let optionalString = assumedString // optional type
+
+/// 하지만 일반 옵셔널로 처리할 수 없는 경우에는 내부 값으로 인해 변경된다.
+let valueString: String = assumedString // 이 경우에는 valueString이 String 타입이므로 값이 벗겨진다.
+
+/*------------------------------------------*/
+
+///- Error Handling
+///
+/// swift의 에러 핸들링은 do 구문 내에서 이루어진다.
+/// 따라서, 발생하는 여러 케이스의 에러에 대해 catch문으로 분기하여 개별적으로 처리할 수 있다.
+
+func canThrowError() throws{
+    
+}
+
+do{
+    try canThrowsError()
+    // no error was thrown
+    catch{
+    // an error was thrown
+    }
+}
+
+func makeASandwich() throws{
+    // ...
+}
+
+do{
+    try makeASandwich()
+    eatSandwich()
+    catch SandwichError.outOfCleanDishes{
+        washDishes()
+    }
+    catch SandwichError.missingIngredients(let buyGroceries(ingredients)
+}
