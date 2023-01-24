0f077411151a3ad7a39459066f3a2f6eb5451f39
LEE <ljhlmkljhlmkljhlmk@naver.com>
2023-01-25 00:22:57 +0900

    Edit Chapter [Nested Types]

--- main.swift
+++ main.swift
@@ -1,4 +1,1 @@
-var testScores = ["Dave" : [91,89,77], "John" : [87,88,88]]
-testScores["Dave"]?[0] += 2 // Dave : [93,89,77]
-testScores["John"]?[2] -= 1 // John : [87,88,86]
-testScores["Lee"]?[3] = 4 // nil
+
